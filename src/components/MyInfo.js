2d311205995b844ac32d56ce8201fbe585f8d464
Laurence Uwins <laurenceuwins@gmail.com>
2020-04-13 19:07:07 +0100

    time stamp: 35:10

---
+++ src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MyInfo(){
+  return(
+    <div>
+      <h1>Laurence Uwins</h1>
+      <p>This is a paragraph about me</p>
+      <ol> 
+        <li>Athins</li>
+        <li>Stockholm</li>
+        <li>Los Angeles</li>
+      </ol>
+    </div>
+  )
+}
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './style.css';
+
+// ReactDOM.render(<div><h1>Hello World!</h1> <p>This is a paragraph</p></div>, document.getElementById('root'));
+
+// // The following will over-write the above, but i am keeping both so that i can view each step
+
+// /////////////////////////////////////////////////////////////////
+// ReactDOM.render(
+//   <ul>
+//     <li> lsit item ett</li>
+//     <li> list item dos</li>
+//     <li> list item drei </li>
+//   </ul>,
+//   document.getElementById('root')
+//   );
+
+// /////////////////////////////////////////////////////////////////
+//   function MyList(){
+//     return(
+//       <ul>
+//         <li>list function 1</li>
+//         <li>list function 2</li>
+//         <li>list function 3</li>
+//       </ul>
+//     )
+//   }
+
+//   ReactDOM.render(
+//     <MyList />,
+//     document.getElementById("root")
+//   );
+
+/////////////////////////////////////////////////////////////////
+import MyInfo from "./components/MyInfo"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <MyInfo />,
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/////////////////////////////////////////////////////////////////
