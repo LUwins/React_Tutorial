66af1c36b149ccc109ef4da33082e4e8fcc1f537
Laurence Uwins <=>
2020-04-16 21:35:36 +0100

    1:29:45

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import App from "./components/App"
+import Todoitem from "./components/TodoItem"
 
 ReactDOM.render(
   <App />,
